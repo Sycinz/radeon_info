cd0fa48f1a0b2206c623922a4e88f53bdb28350c
Sycinz <gronbosof@gmail.com>
2024-02-26 12:02:58 +0100

    Executing working bash command

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
 use std::process::Command;
 
 fn main() {
-    let output = if cfg!(target_os = "linux") {
-        Command::new("zsh")
-            .args(["-c", "echo hello"])
-            .output()
-            .expect("failed to execute process")
-    } else {
-        Command::new("sh")
-            .arg("-c")
-            .arg("echo hello")
-            .output()
-            .expect("failed to execute process")
-    };
+    let output = Command::new("bash")
+        .arg("-c")
+        .arg("echo Hello from Rust!")
+        .output()
+        .expect("Failed to execute command");
 
-    println!("{:?}", output);
+    if output.status.success() {
+        let result = String::from_utf8_lossy(&output.stdout);
+        println!("Command output: {}", result);
+    } else {
+        let result = String::from_utf8_lossy(&output.stderr);
+        eprintln!("Command failed with error: {}", result);
+    }
 }
