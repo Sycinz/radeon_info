4cc065f058fedcbefa4dc1a7652ff8390144fd6f
Sycinz <gronbosof@gmail.com>
2024-02-26 12:52:44 +0100

    Reading freq and temps of GPU

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,42 @@
-use std::process::Command;
+use std::{error, process::Command};
 
 fn main() {
-    println!("This is a radeon_info program in testing version");
+    println!("This is a radeon_info program in testing version\n");
     println!("{}", get_gpu_info("frequency"));
-    println!("{}", get_gpu_info("frequency")); // Other info_types will be added later
-    println!("{}", get_gpu_info("frequency"));
+    println!("{}", get_gpu_info("temperature")); // Other info_types will be added later
 }
 
 fn get_gpu_info(info_type: &str) -> String {
     let mut command = String::from("cat ");
-    let path = String::from("/sys/class/drm/card1/device/pp_dpm_sclk");
+    let mut path = String::new();
 
-    command += &path;
-
+    // Frequency case
     if info_type == "frequency" {
+        path = String::from("/sys/class/drm/card*/device/pp_dpm_sclk");
+
+        command += &path;
+
+        println!("Frequency of GPU: ");
+        let output = Command::new("bash")
+            .arg("-c")
+            .arg(command)
+            .output()
+            .expect("Failed to execute command");
+
+        if output.status.success() {
+            let result = String::from_utf8_lossy(&output.stdout);
+            String::from(result)
+        } else {
+            let result = String::from_utf8_lossy(&output.stderr);
+            String::from(result)
+        }
+    // Temperature case
+    } else if info_type == "temperature" {
+        path = String::from("/sys/class/drm/card*/device/hwmon/hwmon*/temp1_input");
+
+        command += &path;
+
+        println!("Temperature of GPU: ");
         let output = Command::new("bash")
             .arg("-c")
             .arg(command)
@@ -28,6 +51,6 @@ fn get_gpu_info(info_type: &str) -> String {
             String::from(result)
         }
     } else {
-        panic!("PANICCCCC AAAAAAAAAAAAAAAAAA");
+        panic!("Could not find GPU property.");
     }
 }
