c99a089a05be1966ee21e1373287b872d3b9969d
Sycinz <gronbosof@gmail.com>
2024-02-26 12:37:18 +0100

    GPU frequency added

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,33 @@
 use std::process::Command;
 
 fn main() {
-    let output = Command::new("bash")
-        .arg("-c")
-        .arg("cat /sys/power/disk && cat /sys/power/image_size")
-        .output()
-        .expect("Failed to execute command");
+    println!("This is a radeon_info program in testing version");
+    println!("{}", get_gpu_info("frequency"));
+    println!("{}", get_gpu_info("frequency")); // Other info_types will be added later
+    println!("{}", get_gpu_info("frequency"));
+}
 
-    if output.status.success() {
-        let result = String::from_utf8_lossy(&output.stdout);
-        println!("Command output: {}", result);
+fn get_gpu_info(info_type: &str) -> String {
+    let mut command = String::from("cat ");
+    let path = String::from("/sys/class/drm/card1/device/pp_dpm_sclk");
+
+    command += &path;
+
+    if info_type == "frequency" {
+        let output = Command::new("bash")
+            .arg("-c")
+            .arg(command)
+            .output()
+            .expect("Failed to execute command");
+
+        if output.status.success() {
+            let result = String::from_utf8_lossy(&output.stdout);
+            String::from(result)
+        } else {
+            let result = String::from_utf8_lossy(&output.stderr);
+            String::from(result)
+        }
     } else {
-        let result = String::from_utf8_lossy(&output.stderr);
-        eprintln!("Command failed with error: {}", result);
+        panic!("PANICCCCC AAAAAAAAAAAAAAAAAA");
     }
 }
